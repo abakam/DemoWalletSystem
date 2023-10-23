d0af4d72dd11d2797d60c43deb0cdf6033b6fa78
Ab Akam <abrahamakam@gmail.com>
2023-10-23 12:09:31 +0100

    added unit test for FundWallet method of WalletService

--- BetWalletApi.Tests/ServicesTests/WalletServiceTest.cs
+++ BetWalletApi.Tests/ServicesTests/WalletServiceTest.cs
@@ -2,6 +2,7 @@
 using BetWalletApi.DTOs.Responses;
 using BetWalletApi.Models.Common;
 using BetWalletApi.Models.Common.Constants;
+using BetWalletApi.Models.Transactions;
 using BetWalletApi.Models.Users;
 using BetWalletApi.Models.Wallets;
 using BetWalletApi.Services;
@@ -125,5 +126,172 @@ namespace BetWalletApi.Tests.ServicesTests
             Assert.Equal(ResponseStatusCodes.INTERNAL_SERVER_ERROR, actualCreateWalletResponse.ErrorCode);
 
         }
+
+        [Fact]
+        public async Task FundWalletAsync_BaseResponseWithSuccess_ValidInputs()
+        {
+            // Arrange
+            var fundWalletRequest = new FundWalletRequest
+            {
+                Amount = 5000,
+                TransactionType = "Deposit"
+            };
+
+            string username = "abakam";
+            Guid userId = Guid.NewGuid();
+
+            _unitOfWork.Setup(x => x.Users.GetByUsernameAsync(username).Result).Returns(new User { Id = userId });
+            _unitOfWork.Setup(x => x.Wallets.GetByUserIdAsync(userId).Result).Returns(new Wallet { });
+            _unitOfWork.Setup(x => x.Transactions.Add(It.IsAny<Transaction>())).Verifiable();
+
+            var walletService = new WalletService(_unitOfWork.Object, _logger);
+
+            // Act
+            var actualFundWalletResponse = await walletService.FundWalletAsync(username, fundWalletRequest);
+
+            // Assert
+            Assert.IsType<BaseResponse<FundWalletResponse>>(actualFundWalletResponse);
+            Assert.True(actualFundWalletResponse.Success);
+            Assert.True(String.IsNullOrWhiteSpace(actualFundWalletResponse.Message));
+            Assert.True(String.Equals(username, actualFundWalletResponse.Result.Username));
+            Assert.True(String.Equals(fundWalletRequest.TransactionType, actualFundWalletResponse.Result.TransactionType));
+            Assert.NotEqual(Guid.Empty, actualFundWalletResponse.Result.TransactionId);
+            Assert.Equal(fundWalletRequest.Amount, actualFundWalletResponse.Result.Amount, 2);
+
+        }
+
+        [Fact]
+        public async Task FundWalletAsync_BaseResponseWithError_InvalidTransactionTypeInput()
+        {
+            // Arrange
+            var fundWalletRequest = new FundWalletRequest
+            {
+                Amount = 5000,
+                TransactionType = "Bet"
+            };
+
+            string username = "abakam";
+         
+
+            var walletService = new WalletService(_unitOfWork.Object, _logger);
+
+            // Act
+            var actualFundWalletResponse = await walletService.FundWalletAsync(username, fundWalletRequest);
+
+            // Assert
+            Assert.IsType<BaseResponse<FundWalletResponse>>(actualFundWalletResponse);
+            Assert.False(actualFundWalletResponse.Success);
+            Assert.True(String.Equals(ErrorMessages.INVALID_TRANSACTION_TYPE, actualFundWalletResponse.Message));
+            Assert.Equal(ResponseStatusCodes.BAD_REQUEST, actualFundWalletResponse.ErrorCode);
+
+        }
+
+        [Fact]
+        public async Task FundWalletAsync_BaseResponseWithError_NegativeAmountInput()
+        {
+            // Arrange
+            var fundWalletRequest = new FundWalletRequest
+            {
+                Amount = -5000,
+                TransactionType = "Deposit"
+            };
+
+            string username = "abakam";
+
+
+            var walletService = new WalletService(_unitOfWork.Object, _logger);
+
+            // Act
+            var actualFundWalletResponse = await walletService.FundWalletAsync(username, fundWalletRequest);
+
+            // Assert
+            Assert.IsType<BaseResponse<FundWalletResponse>>(actualFundWalletResponse);
+            Assert.False(actualFundWalletResponse.Success);
+            Assert.True(String.Equals(ErrorMessages.INVALID_AMOUNT, actualFundWalletResponse.Message));
+            Assert.Equal(ResponseStatusCodes.BAD_REQUEST, actualFundWalletResponse.ErrorCode);
+
+        }
+
+        [Fact]
+        public async Task FundWalletAsync_BaseResponseWithError_UserDoNotExists()
+        {
+            // Arrange
+            var fundWalletRequest = new FundWalletRequest
+            {
+                Amount = 5000,
+                TransactionType = "Deposit"
+            };
+
+            string username = "abakam";
+            User nullUser = null;
+
+            _unitOfWork.Setup(x => x.Users.GetByUsernameAsync(username).Result).Returns(nullUser);
+            var walletService = new WalletService(_unitOfWork.Object, _logger);
+
+            // Act
+            var actualFundWalletResponse = await walletService.FundWalletAsync(username, fundWalletRequest);
+
+            // Assert
+            Assert.IsType<BaseResponse<FundWalletResponse>>(actualFundWalletResponse);
+            Assert.False(actualFundWalletResponse.Success);
+            Assert.True(String.Equals(ErrorMessages.WALLET_DO_NOT_EXIST, actualFundWalletResponse.Message));
+            Assert.Equal(ResponseStatusCodes.Not_Found, actualFundWalletResponse.ErrorCode);
+
+        }
+
+        [Fact]
+        public async Task FundWalletAsync_BaseResponseWithError_UserDoNotHaveWallet()
+        {
+            // Arrange
+            var fundWalletRequest = new FundWalletRequest
+            {
+                Amount = 5000,
+                TransactionType = "Deposit"
+            };
+
+            string username = "abakam";
+            Wallet nullWallet = null;
+
+            _unitOfWork.Setup(x => x.Users.GetByUsernameAsync(username).Result).Returns(new User { Id = Guid.NewGuid()});
+            _unitOfWork.Setup(x => x.Wallets.GetByUserIdAsync(Guid.NewGuid()).Result).Returns(nullWallet);
+            var walletService = new WalletService(_unitOfWork.Object, _logger);
+
+            // Act
+            var actualFundWalletResponse = await walletService.FundWalletAsync(username, fundWalletRequest);
+
+            // Assert
+            Assert.IsType<BaseResponse<FundWalletResponse>>(actualFundWalletResponse);
+            Assert.False(actualFundWalletResponse.Success);
+            Assert.True(String.Equals(ErrorMessages.WALLET_DO_NOT_EXIST, actualFundWalletResponse.Message));
+            Assert.Equal(ResponseStatusCodes.Not_Found, actualFundWalletResponse.ErrorCode);
+
+        }
+
+        [Fact]
+        public async Task FundWalletAsync_BaseResponseWithError_InternalServerError()
+        {
+            // Arrange
+            var fundWalletRequest = new FundWalletRequest
+            {
+                Amount = 5000,
+                TransactionType = "Deposit"
+            };
+
+            string username = "abakam";
+
+            _unitOfWork.Setup(x => x.Users.GetByUsernameAsync(username).Result).Throws(new Exception());
+            var walletService = new WalletService(_unitOfWork.Object, _logger);
+
+            // Act
+            var actualFundWalletResponse = await walletService.FundWalletAsync(username, fundWalletRequest);
+
+            // Assert
+            Assert.IsType<BaseResponse<FundWalletResponse>>(actualFundWalletResponse);
+            Assert.False(actualFundWalletResponse.Success);
+            Assert.True(String.Equals(ErrorMessages.INTERNAL_ERROR_MESSAGE, actualFundWalletResponse.Message));
+            Assert.Equal(ResponseStatusCodes.INTERNAL_SERVER_ERROR, actualFundWalletResponse.ErrorCode);
+
+        }
+
     }
 }
